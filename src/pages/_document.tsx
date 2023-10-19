9c9341c5b802c90af549743df0764733a1f583f7
Asadullo <72871274+Asadullo2502@users.noreply.github.com>
2023-10-20 00:26:45 +0500

    starter my portfolio

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,46 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="apple-touch-icon" href="/favicon.ico" sizes="48x48" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/favicon.ico"
+          sizes="48x48"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/48x48.png" sizes="48x48" />
+        <link rel="apple-touch-icon" href="/48x48.png" sizes="48x48" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/48x48.png"
+          sizes="48x48"
+        />
+        <link rel="shortcut icon" href="/48x48.png" sizes="48x48" />
+        <link rel="icon" href="/96x96.png" sizes="96x96" />
+        <link rel="apple-touch-icon" href="/96x96.png" sizes="96x96" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/96x96.png"
+          sizes="96x96"
+        />
+        <link rel="shortcut icon" href="/96x96.png" sizes="96x96" />
+        <link rel="icon" href="/144x144.png" sizes="144x144" />
+        <link rel="apple-touch-icon" href="/144x144.png" sizes="144x144" />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/144x144.png"
+          sizes="144x144"
+        />
+        <link rel="shortcut icon" href="/144x144.png" sizes="144x144" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,31 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/containers/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
-      main: '#FFFFFF',
-      primary: '#10172A',
+      primary: '#FFFFFF',
+      main: '#10172A',
       secondary: '#64748B',
+      addition: '#58dad9',
+      dark: '#0B101E',
       background: '#F1F5F9',
     },
-    extend: {},
+    screens: {
+      mobile: '375px',
+      tablet: '768px',
+      laptop: '1024px',
+    },
+    extend: {
+      boxShadow: {
+        hovered: 'inset 0px 0px 1rem 0px #58dad9, 0px 0px 1rem 0px #58dad9',
+      },
+    },
   },
   plugins: [],
 };
