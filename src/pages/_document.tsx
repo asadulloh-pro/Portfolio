c856c6a1be28ae0688ebaa547334b0abe223c1a0
Asadulloh <asadullosadirdinov@gmail.com>
2023-10-21 01:39:14 +0500

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,22 +12,22 @@ export default function Document() {
           sizes="48x48"
         />
         <link rel="shortcut icon" href="/favicon.ico" sizes="48x48" />
-        <link rel="icon" href="/48x48.png" sizes="48x48" />
-        <link rel="apple-touch-icon" href="/48x48.png" sizes="48x48" />
+        <link rel="icon" href="/32x32.png" sizes="48x48" />
+        <link rel="apple-touch-icon" href="/32x32.png" sizes="48x48" />
         <link
           rel="apple-touch-icon-precomposed"
-          href="/48x48.png"
+          href="/32x32.png"
           sizes="48x48"
         />
-        <link rel="shortcut icon" href="/48x48.png" sizes="48x48" />
-        <link rel="icon" href="/96x96.png" sizes="96x96" />
+        <link rel="shortcut icon" href="/32x32.png" sizes="48x48" />
+        <link rel="icon" href="/144x144.png" sizes="96x96" />
         <link rel="apple-touch-icon" href="/96x96.png" sizes="96x96" />
         <link
           rel="apple-touch-icon-precomposed"
           href="/96x96.png"
           sizes="96x96"
         />
-        <link rel="shortcut icon" href="/96x96.png" sizes="96x96" />
+        <link rel="shortcut icon" href="/144x144.png" sizes="96x96" />
         <link rel="icon" href="/144x144.png" sizes="144x144" />
         <link rel="apple-touch-icon" href="/144x144.png" sizes="144x144" />
         <link
